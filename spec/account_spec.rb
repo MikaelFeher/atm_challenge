bbb97ba0aa30b33a48cad0b1c2e4967b2045f4f2
MikaelFeher <mikael.feher@gmail.com>
2016-06-14 16:16:40 +0200

    added account status on initialize

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -15,7 +15,7 @@ describe Account do
   it 'account active on initialize?' do
     expect(subject.account_status).to eq :active
   end
-  
+
   it 'pin exists on initialize' do
     expect(subject.pin_code).not_to eq nil
   end
@@ -23,5 +23,10 @@ describe Account do
   it 'make sure pin is 4 digits' do
     pin_length = Math.log10(subject.pin_code).to_i + 1
       expect(pin_length).to eq 4
+  end
+
+  it 'deactivates the account using instance method' do
+    subject.deactivate
+    expect(subject.account_status).to eq :deactivated
   end
 end
