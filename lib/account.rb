67f761623d1edcb1220b7fa52c30aaa8cabec9a5
MikaelFeher <mikael.feher@gmail.com>
2016-06-14 15:15:44 +0200

    added check for pin to not be nil and an expiry date method as well as test

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,22 @@
 class Account
 
   attr_reader :pin_code
+  attr_reader :balance
+  attr_reader :account_status
+  attr_reader :exp_date
+
+  STANDARD_VALIDITY_YEARS = 5
 
   def initialize
     @pin_code = rand(1000..9999)
     @balance = 100
+    @exp_date = set_exp_date
     @account_status = :active
 
   end
 
-=begin
-  def new_account(account_owner)
-
-  def pin_code
-    pin_code =
+  def set_exp_date
+    Date.today.next_year(STANDARD_VALIDITY_YEARS).strftime("%m/%y")
   end
-=end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,12 +2,17 @@ require './lib/account.rb'
 
 describe Account do
 
-=begin
+
   it 'is expected to have an expiry date on initialize' do
     expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
   end
-=end
+
+
+
+  it 'pin exists on initialize' do
+    expect(subject.pin_code).not_to eq nil
+  end
 
   it 'make sure pin is 4 digits' do
     pin_length = Math.log10(subject.pin_code).to_i + 1
